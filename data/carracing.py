2d1b8f3744989f546e7bb24a8f530304981af657
ctallec <corentinxtallec@gmail.com>
2018-05-07 19:30:36 +0200

    [TEST] Testing webhook.

--- data/carracing.py
+++ data/carracing.py
@@ -3,3 +3,7 @@ Generating data from the CarRacing gym environment.
 !!! DOES NOT WORK ON TITANIC, DO IT AT HOME, THEN SCP !!!
 """
 import gym
+
+def generate_data(rollouts):
+    """ Generates data (As a Packed Sequence Maybe?) """
+    pass
