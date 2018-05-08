7edc69a2a16bb2da111c7d8ce3595ff625b2971e
ctallec <corentinxtallec@gmail.com>
2018-05-08 11:38:25 +0200

    [ADD] Data generating script

--- data/carracing.py
+++ data/carracing.py
@@ -2,8 +2,37 @@
 Generating data from the CarRacing gym environment.
 !!! DOES NOT WORK ON TITANIC, DO IT AT HOME, THEN SCP !!!
 """
+import argparse
+from os.path import join, exists
 import gym
+import numpy as np
 
-def generate_data(rollouts):
+def generate_data(rollouts, data_dir):
     """ Generates data (As a Packed Sequence Maybe?) """
-    pass
+    assert exists(data_dir), "The data directory does not exist..."
+
+    env = gym.make("CarRacing-v0")
+    for i in range(rollouts):
+        s = env.reset()
+        env.env.viewer.window.dispatch_events()
+        s_rollout = []
+        r_rollout = []
+        while True:
+            action = env.action_space.sample()
+            s, r, done, _ = env.step(action)
+            env.env.viewer.window.dispatch_events()
+            s_rollout += [s]
+            r_rollout += [r]
+            if done:
+                print("> End of rollout {}, {} frames...".format(i, len(s_rollout)))
+                np.savez(join(data_dir, 'rollout_{}'.format(i)),
+                         observations=np.array(s_rollout),
+                         rewards=np.array(r_rollout))
+                break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rollouts', type=int, help="Number of rollouts")
+    parser.add_argument('--dir', type=str, help="Where to place rollouts")
+    args = parser.parse_args()
+    generate_data(args.rollouts, args.dir)
---
+++ data/generation_script.py
@@ -0,0 +1,35 @@
+"""
+Encapsulate generate data to make it parallel
+"""
+from os import makedirs
+from os.path import join
+import argparse
+from multiprocessing import Pool
+from subprocess import call
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rollouts', type=int,
+                    help="Total number of rollouts.")
+parser.add_argument('--threads', type=int,
+                    help="Number of threads")
+parser.add_argument('--rootdir', type=str,
+                    help="Directory to store rollout "
+                    "directories of each thread")
+args = parser.parse_args()
+
+rpt = args.rollouts // args.threads + 1
+
+def _threaded_generation(i):
+    tdir = join(args.rootdir, 'thread_{}'.format(i))
+    makedirs(tdir, exist_ok=True)
+    cmd = ['xvfb-run', '-s', '"-screen 0 1400x900x24"']
+    cmd += ['--server-num={}'.format(i + 1)]
+    cmd += ["python", "data/carracing.py", "--dir",
+            tdir, "--rollouts", str(rpt)]
+    cmd = " ".join(cmd)
+    print(cmd)
+    call(cmd, shell=True)
+    return True
+
+with Pool(args.threads) as p:
+    p.map(_threaded_generation, range(args.threads))
