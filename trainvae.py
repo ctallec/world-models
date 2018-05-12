c895535c38e1059956c381f9fbf7ec2cec0ebb42
Leonard Blier <lblier@pacific-1.saclay.inria.fr>
2018-05-12 19:03:15 +0200

    File loading and vae training

--- trainvae.py
+++ trainvae.py
@@ -1,4 +1,11 @@
 import argparse
+import numpy as np
+import os
+from os.path import isdir, join
+
+from tqdm import tqdm
+
+
 import torch
 import torch.utils.data
 from torch import nn, optim
@@ -10,7 +17,7 @@ from torchvision.utils import save_image
 from models.vae import Encoder, Decoder
 from itertools import chain
 
-parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser = argparse.ArgumentParser(description='VAE Trainer')
 parser.add_argument('--batch-size', type=int, default=32, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=100, metavar='N',
@@ -29,37 +36,83 @@ device = torch.device("cuda" if args.cuda else "cpu")
 
 
 transform_train = transforms.Compose([
-    transforms.RandomCrop(64, pad_if_needed=True),
+    transforms.ToPILImage(),
+    transforms.Resize((64, 64)),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
 ])
 
 transform_test = transforms.Compose([
-    transforms.RandomCrop(64, pad_if_needed=True),
+    transforms.ToPILImage(),
+    transforms.Resize((64, 64)),
     transforms.ToTensor(),
 ])
 
-trainset = datasets.MNIST(root='/data/titanic_1/datasets', train=True,
-                            download=True, transform=transform_train)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                           shuffle=False, num_workers=2)
+# TODO : We should not concatenate all np arrays !
 
-testset = datasets.MNIST(root='/data/titanic_1/datasets', train=False,
-                           download=True, transform=transform_test)
-test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
-                                          shuffle=False, num_workers=2)
+class RolloutObservationDataset(torch.utils.data.Dataset):
+    def __init__(self, root, extension, transform, buffersize):
+        self.tranform = transform
+        self.buffersize = buffersize
+
+        subdirs = [join(root, sd)  for sd in os.listdir(root) if isdir(join(root,sd))]
+        self.filenames = [join(sd, fn) for sd in subdirs for fn in os.listdir(sd) if fn.endswith(extension)]
+        self.countfiles = 0
+        self.countsamplesbuffer = 0
+        
+    def load_buffer(self):
+        filebuffer = self.filenames[self.countfiles:self.countfiles+self.buffersize]
+        obslist = []
+        pbar = tqdm(total=len(filebuffer),
+                    bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} {postfix}')
+        pbar.set_description("Loading file buffer ...")
+        for fn in filebuffer:
+            with np.load(fn) as data:
+                obslist.append(data['observations'])
+            pbar.update(1)
+        self.observations = np.concatenate(obslist)
+        pbar.close()
+    def __len__(self):
+        return self.observations.shape[0]
+
+    def __getitem__(self, i):
+        self.countsamplesbuffer += 1
+        return self.tranform(self.observations[i])
+
+        
+def observationloader(filename):
+    with np.load(filename) as data:
+        observation = data['observations']
+    idx = np.random.randint(observation.shape[0])
+    return observation[idx]
+        
 
 
+# dataset_train = datasets.DatasetFolder('/data/titanic_4/datasets/carracing/rollouts/',
+#                                        observationloader, 'npz', transform = transform_train)
 
+# dataset_test = datasets.DatasetFolder('/data/titanic_4/datasets/carracing/rollouts/',
+#                                       observationloader, 'npz', transform = transform_test)
+# train_loader = torch.utils.data.DataLoader(dataset_train,
+#     batch_size=args.batch_size, shuffle=True, num_workers=2)
+# test_loader = torch.utils.data.DataLoader(dataset_test,
+#     batch_size=args.batch_size, shuffle=True, num_workers=2)
 
-
+dataset_train = RolloutObservationDataset('/data/titanic_4/datasets/carracing/rollouts/',
+                                          'npz', transform_train, 200)
+dataset_test = RolloutObservationDataset('/data/titanic_4/datasets/carracing/rollouts/',
+                                          'npz', transform_test, 10)
+train_loader = torch.utils.data.DataLoader(dataset_train,
+    batch_size=args.batch_size, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(dataset_test,
+    batch_size=args.batch_size, shuffle=True, num_workers=2)
 
 
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
-        self.encoder = Encoder(1, 32)
-        self.decoder = Decoder(1, 32)
+        self.encoder = Encoder(3, 32)
+        self.decoder = Decoder(3, 32)
 
     def forward(self, x):
         mu, logsigma = self.encoder(x)
@@ -89,8 +142,9 @@ def loss_function(recon_x, x, mu, logsigma):
 
 def train(epoch):
     model.train()
+    dataset_train.load_buffer()
     train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    for batch_idx, data in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
@@ -110,9 +164,10 @@ def train(epoch):
 
 def test(epoch):
     model.eval()
+    dataset_test.load_buffer()
     test_loss = 0
     with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
+        for i, data in enumerate(test_loader):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
             test_loss += loss_function(recon_batch, data, mu, logvar).item()
@@ -133,5 +188,5 @@ for epoch in range(1, args.epochs + 1):
     with torch.no_grad():
         sample = torch.randn(64, 32).to(device)
         sample = model.decoder(sample).cpu()
-        save_image(sample.view(64, 1, 64, 64),
+        save_image(sample.view(64, 3, 64, 64),
                    'results/sample_' + str(epoch) + '.png')
