910ea02f22ba58389967dd5ee3b021d621b99ea1
ctallec <corentinxtallec@gmail.com>
2018-05-18 09:50:41 +0200

    [ADD] data examination + continuous policy sampling

--- examine_data.py
+++ examine_data.py
@@ -1,17 +1,57 @@
 """ Some data examination """
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
-from data.utils import RolloutSequenceDataset
+import gym
+from gym.envs.box2d.car_racing import FPS
 
-dataloader = DataLoader(
-    RolloutSequenceDataset(
-        root='datasets/carracing', seq_len=256,
-        transform=lambda x: x, buffer_size=10),
-    batch_size=1)
+def plot_rollout():
+    """ Plot a rollout """
+    from torch.utils.data import DataLoader
+    from data.utils import RolloutSequenceDataset
+    dataloader = DataLoader(
+        RolloutSequenceDataset(
+            root='datasets/carracing', seq_len=900,
+            transform=lambda x: x, buffer_size=10,
+            train=False),
+        batch_size=1, shuffle=True)
 
-monitor = plt.imshow(np.zeros((64, 64, 3)))
-for data in dataloader:
-    obs = data[0].numpy().unsqueeze()
-    monitor.set_data(obs)
-    plt.pause(.1)
+    dataloader.dataset.load_next_buffer()
+
+    monitor = plt.imshow(np.zeros((64, 64, 3)))
+    for data in dataloader:
+        obs_seq = data[0].numpy().squeeze()
+        action_seq = data[1].numpy().squeeze()
+        for obs, action in zip(obs_seq, action_seq):
+            monitor.set_data(obs)
+            print(action)
+            plt.pause(.01)
+        break
+
+def sample_continuous_policy(action_space, seq_len, dt):
+    """
+    Sample a continuous policy. Atm, action_space is supposed
+    to be a box environment.
+    """
+    actions = [action_space.sample()]
+    for _ in range(seq_len):
+        daction_dt = np.random.randn(*actions[-1].shape)
+        actions.append(
+            np.clip(actions[-1] + math.sqrt(dt) * daction_dt,
+                    action_space.low, action_space.high))
+    return actions
+
+def plot_continuous_policy():
+    """ Plot a rollout using a continuous policy """
+    episode_length = 1000
+    env = gym.make('CarRacing-v0')
+    env.reset()
+
+    actions = sample_continuous_policy(env.action_space, episode_length, 1 / FPS)
+
+    for i in range(episode_length):
+        env.step(actions[i])
+        env.render()
+
+if __name__ == '__main__':
+    plot_continuous_policy()
