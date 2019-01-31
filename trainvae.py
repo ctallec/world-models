e212b12f735948522568dc9dcd581d08bb2bbf35
Leonard Blier <leonard.blier@gmail.com>
2019-01-31 05:46:53 -0800

    before debug with francisco

--- trainvae.py
+++ trainvae.py
@@ -19,6 +19,9 @@ from utils.learning import EarlyStopping
 from utils.learning import ReduceLROnPlateau
 from data.loaders import RolloutObservationDataset
 
+
+import ipdb
+
 parser = argparse.ArgumentParser(description='VAE Trainer')
 parser.add_argument('--batch-size', type=int, default=32, metavar='N',
                     help='input batch size for training (default: 32)')
@@ -35,10 +38,10 @@ args = parser.parse_args()
 cuda = torch.cuda.is_available()
 
 
-torch.manual_seed(123)
+# torch.manual_seed(123)
 
 device = torch.device("cuda" if cuda else "cpu")
-
+# device = torch.device("cpu")
 
 transform_train = transforms.Compose([
     transforms.ToPILImage(),
@@ -47,37 +50,47 @@ transform_train = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-transform_test = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((RED_SIZE, RED_SIZE)),
-    transforms.ToTensor(),
-])
+# transform_test = transforms.Compose([
+#     transforms.ToPILImage(),
+#     transforms.Resize((RED_SIZE, RED_SIZE)),
+#     transforms.ToTensor(),
+# ])
 
 dataset_train = RolloutObservationDataset('datasets/carracing',
                                           transform_train, train=True)
-dataset_test = RolloutObservationDataset('datasets/carracing',
-                                         transform_test, train=False)
+# dataset_test = RolloutObservationDataset('datasets/carracing',
+#                                          transform_test, train=False)
 train_loader = torch.utils.data.DataLoader(
     dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=2)
-test_loader = torch.utils.data.DataLoader(
-    dataset_test, batch_size=args.batch_size, shuffle=True, num_workers=2)
+# test_loader = torch.utils.data.DataLoader(
+#     dataset_test, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
 
 model = VAE(3, LSIZE).to(device)
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters())#, lr=1e-4, betas=(0.5, 0.9))
+# optimizer = optim.SGD(model.parameters(), lr=1e-3)
 scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5)
 earlystopping = EarlyStopping('min', patience=30)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logsigma):
     """ VAE loss function """
-    BCE = F.mse_loss(recon_x, x, size_average=False)
+    # BCE = F.mse_loss(recon_x, x, size_average=False)
+    # ipdb.set_trace()
+    recon_x = recon_x.cpu()
+    x = x.cpu()
+    mu = mu.cpu()
+    logsigma = logsigma.cpu()
+    BCE = .5 * (recon_x - x).pow(2).sum(dim=(1,2,3)).sum()
+    # BCE = (recon_x - x).pow(2).sum(dim=1).sum()
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + 2 * logsigma - mu.pow(2) - (2 * logsigma).exp())
+    # print(logsigma.sum())
+    KLD = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp(), dim=1).sum()
+    # print(f"BCE:{BCE.item():.0e},KLD:{KLD.item():.0e}")
     return BCE + KLD
 
 
@@ -87,13 +100,15 @@ def train(epoch):
     dataset_train.load_next_buffer()
     train_loss = 0
     for batch_idx, data in enumerate(train_loader):
+
         data = data.to(device)
-        optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
         loss = loss_function(recon_batch, data, mu, logvar)
+        optimizer.zero_grad()
         loss.backward()
-        train_loss += loss.item()
         optimizer.step()
+        train_loss += loss.item()
+        
         if batch_idx % 20 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -126,25 +141,25 @@ if not exists(vae_dir):
     mkdir(join(vae_dir, 'samples'))
 
 reload_file = join(vae_dir, 'best.tar')
-if not args.noreload and exists(reload_file):
-    state = torch.load(reload_file)
-    print("Reloading model at epoch {}"
-          ", with test error {}".format(
-              state['epoch'],
-              state['precision']))
-    model.load_state_dict(state['state_dict'])
-    optimizer.load_state_dict(state['optimizer'])
-    scheduler.load_state_dict(state['scheduler'])
-    earlystopping.load_state_dict(state['earlystopping'])
+# if not args.noreload and exists(reload_file):
+#     state = torch.load(reload_file)
+#     print("Reloading model at epoch {}"
+#           ", with test error {}".format(
+#               state['epoch'],
+#               state['precision']))
+#     model.load_state_dict(state['state_dict'])
+#     optimizer.load_state_dict(state['optimizer'])
+#     scheduler.load_state_dict(state['scheduler'])
+#     earlystopping.load_state_dict(state['earlystopping'])
 
 
 cur_best = None
 
 for epoch in range(1, args.epochs + 1):
     train(epoch)
-    test_loss = test()
-    scheduler.step(test_loss)
-    earlystopping.step(test_loss)
+    # test_loss = test()
+    # scheduler.step(test_loss)
+    # earlystopping.step(test_loss)
 
     # checkpointing
     best_filename = join(vae_dir, 'best.tar')
